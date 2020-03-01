f7a17af1799179d5476483ed46fd915f729a3fb3
AdamDyjurMclean <55033999+AdamDyjurMclean@users.noreply.github.com>
2020-03-01 15:08:10 -0700

    Add files via upload

---
+++ node_videos/10/src/dictionary-routes.test.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dictionaryRoutes = require("./dictionary-routes");
+const {save} = require("./lib");
+const request = require("supertest");
+
+jest.mock("./lib", () => ({
+    save: jest.fn()
+}));
+
+jest.mock("../data/skiTerms.json", () => [
+    {term: "aaa", defined: "test a"},
+    {term: "bbb", defined: "test b"},
+    {term: "ccc", defined: "test c"}
+]);
+
+const app = express();
+
+app.use("/dictionary", dictionaryRoutes);
+
+describe("dictionary-routes", () => {
+    it("GET /dictionary - sucsess", async () => {
+       const {body} = await request(app).get("/dictionary");
+       expect(body).toEqual([
+        {term: "aaa", defined: "test a"},
+        {term: "bbb", defined: "test b"},
+        {term: "ccc", defined: "test c"}
+    ]);
+    });
+
+    it("DELETE /dictionary/bbb - sucsess", async () => {
+        const {body} = await request(app).delete("/dictionary/bbb");
+        expect(body).toEqual({
+            status: "success",
+            removed: 'bbb',
+            newLength: 2
+          });
+          expect(save).toHaveBeenCalledWith([
+            {term: "aaa", defined: "test a"},
+            {term: "ccc", defined: "test c"}
+        ])
+    });
+});
